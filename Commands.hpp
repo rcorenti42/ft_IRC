86b835fcb991f67ed8f0289dd8247d64cc2cc09b
Sofiane BOUATTOUR <sobouatt@e1r11p5.clusters.42paris.fr>
2022-09-21 00:17:50 +0200

    it compile now

--- Commands.hpp
+++ Commands.hpp
@@ -6,7 +6,7 @@
 /*   By: sobouatt <sobouatt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2022/09/21 00:07:03 by sobouatt         ###   ########.fr       */
+/*   Updated: 2022/09/21 00:17:30 by sobouatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ public:
 		std::cout << "}" << std::endl;
 		std::cout << "message : " << this->_message;
 	}
-	Commands(std::string str, Client *client, Server *server) : _client(client), _server(server)
+	Commands(Client *client, Server *server, std::string str) : _client(client), _server(server)
 	{
 		size_t pos;
 		if ((pos = str.find(":")) != std::string::npos) {
@@ -70,41 +70,6 @@ public:
 		std::cout << "}" << std::endl;
 		std::cout << "message : " << this->_message;
 	}
-
-	// Commands(std::string message)
-	// {
-	// 	size_t pos = 0;
-	// 	bool mess = 0;
-		
-		// while ((pos = message.find(" ")) != std::string::npos)
-		// {
-		// 	this->_args.push_back(message.substr(0, pos));
-		// 	message.erase(0, pos + 1);
-		// }
-	// 	_command = *(_args.begin());
-	// 	this->_args.erase(this->_args.begin());
-	// 	for (std::vector<std::string>::iterator it = this->_args.begin(); it != this->_args.end(); it++)
-	// 	{
-	// 		if (mess)
-	// 		{
-	// 			_message += (*it);
-	// 			this->_args.erase(it);	
-	// 		}		
-	// 		if ((pos = (*it).find(":")) != std::string::npos)
-	// 		{
-	// 			_message += (*it).substr(pos + 1);
-	// 			(*it).erase(pos);
-	// 			mess = 1;
-	// 		}
-	// 	}
-	// 	std::cout << "command : " << this->_command << std::endl;
-	// 	std::cout << "args : {";
-		
-	// 	for (std::vector<std::string>::iterator it = this->_args.begin(); it != this->_args.end(); it++)
-	// 		std::cout << (*it) << ", ";
-	// 	std::cout << "}" << std::endl;
-	// 	std::cout << "message : " << this->_message;
-	// };
 
 	std::string					getCommand() const {
 		return this->_command;
