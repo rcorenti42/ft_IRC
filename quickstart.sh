72ecaecaccf1b7bebc93a3c29cc0891d6eb31b84
Richard CORENTIN <rcorenti@e2r11p18.clusters.42paris.fr>
2022-10-19 02:10:11 +0200

    very/quickstart hostname auto

--- quickstart.sh
+++ quickstart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+hostname=$(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -n 2p)
 if [ $# != 3 ]
 then
     echo "usage: $0 <port> <password> <nickname>"
     exit 0
 fi
-irssi -c localhost -p $1 -w $2 -n $3
+irssi -c localhost -p $1 -w $2 -n $3 -h hostname
--- veryquickstart.sh
+++ veryquickstart.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-irssi -c localhost -p 4243 -w lol
+hostname=$(ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -n 2p)
+irssi -c localhost -p 4243 -w lol -h hostname
